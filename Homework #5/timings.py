254d4373a75d35f8e112eda5fa65b714bd9fc005
Andrew Walker <awalker88@me.com>
2018-07-31 09:51:10 -0500

    deleted redundancies

--- Homework #5/timings.py
+++
@@ -1,5 +0,0 @@
-""" Times how long it takes for insertionSort.py and heapSort.py to run"""
-
-def main():
-    numToSortInsertion = 0
-    numToSortHeap = 0
--- Homework #6/avl_tree_node.py
+++
@@ -1,7 +0,0 @@
-from tree_node import TreeNode
-
-class AVLTreeNode(TreeNode):
-    def __init__(self,key,val,left=None,right=None,
-                 parent=None, balanceFactor=0):
-        TreeNode.__init__(self,key,val,left,right,parent)
-        self.balanceFactor = balanceFactor
